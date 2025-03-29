d01c8ccca28cb3adc509059d70538c15d571b23f
bluncan <bogdan.luncan@analog.com>
2025-03-29 13:35:40 +0200

    added base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bluncan15.github.io',
+  base: 'sitetoni',
   integrations: [react()],
 
   vite: {
